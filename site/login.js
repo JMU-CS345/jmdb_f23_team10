f77200a4cec948217f6889786d7a1bc0b4b72efe
ThomasC02 <cantretw@dukes.jmu.edu>
2023-11-14 15:29:47 -0500

    update login files

---
+++ site/login.js
@@ -0,0 +1,5 @@
+const API_KEY = "api_key=f61be177e52665e7c5e6973bb615e517";
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const API_URL = BASE_URL + 'movie/popular?' + API_KEY;
+
