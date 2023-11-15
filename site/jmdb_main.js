1ade820ea22500c267ee8872bf79ac7fd10501ce
ThomasC02 <cantretw@dukes.jmu.edu>
2023-11-15 15:40:48 -0500

    Login broken

--- site/jmdb_main.js
+++ site/jmdb_main.js
@@ -2,6 +2,8 @@ const API_KEY = "api_key=f61be177e52665e7c5e6973bb615e517";
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const API_URL = BASE_URL + 'movie/popular?' + API_KEY;
+const KEYVAL_API_KEY="EV9B4A3DLp";
+let main = new Keyval(KEYVAL_API_KEY);
 
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
